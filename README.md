577b9719b61b8f8b496d7f0db0d27df188d8bc30
akseyh <yilmazsems@gmail.com>
2025-01-19 23:45:20 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Bear MCP Server
+
+This project is a Model Context Protocol (MCP) server that provides access to the [Bear Notes](https://bear.app).
+
+Bear stores notes on SQLite database. This MCP server runs some SQL commands to access this notes.
+https://bear.app/faq/where-are-bears-notes-located
+
+## Features
+
+- Read notes
+- Search notes by text
+- List all tags
+
+## Installation
+
+```bash
+# Clone the project
+git clone https://github.com/akseyh/bear-mcp-server
+
+# Change directory
+cd bear-mcp-server
+
+# Install dependencies
+npm install
+
+# Build the project
+npm run build
+```
+
+## Claude Desktop Config
+
+Update your claude_desktop_config.json
+
+```json
+{
+  "mcpServers": {
+    "bear": {
+      "command": "node",
+      "args": [".../build/index.js"] // Change it with your path
+    }
+  }
+}
+```
+
+When the server is started, the following MCP tools become available:
+
+- `get_notes`: Retrieves all notes
+- `get_tags`: Lists all tags
+- `get_notes_like`: Searches for notes containing specific text
+
+## Requirements
+
+- Node.js
+- Bear note application (macOS)
+- Access to Bear database
+
+## License
+
+ISC
---
+++ src/utils.ts
@@ -0,0 +1,98 @@
+import sqlite3 from "sqlite3";
+import path from "path";
+import os from "os";
+
+type Note = {
+  ZCREATIONDATE: number;
+  ZSUBTITLE: string;
+  ZTEXT: string;
+  ZTITLE: string;
+  ZUNIQUEIDENTIFIER: string;
+};
+
+type Tag = {
+  ZTITLE: string;
+};
+
+const bearDBPath = path.join(
+  os.homedir(),
+  "Library",
+  "Group Containers",
+  "9K33E3U3T4.net.shinyfrog.bear",
+  "Application Data",
+  "database.sqlite"
+);
+
+const db = new sqlite3.Database(bearDBPath);
+
+export function getNotes() {
+  return new Promise((resolve, reject) => {
+    const notes: Note[] = [];
+
+    db.each(
+      "SELECT * FROM ZSFNOTE WHERE ZARCHIVED=0;",
+      (error, row: Note) => {
+        if (!error) {
+          notes.push({
+            ZCREATIONDATE: row.ZCREATIONDATE,
+            ZSUBTITLE: row.ZSUBTITLE,
+            ZTEXT: row.ZTEXT,
+            ZTITLE: row.ZTITLE,
+            ZUNIQUEIDENTIFIER: row.ZUNIQUEIDENTIFIER,
+          });
+        }
+      },
+      (error, count) => {
+        if (error) reject(error);
+
+        resolve(notes);
+      }
+    );
+  });
+}
+
+export function getNotesLike(like: string) {
+  return new Promise((resolve, reject) => {
+    const notes: Note[] = [];
+
+    db.each(
+      `SELECT * FROM ZSFNOTE WHERE ZARCHIVED=0 AND (ZTEXT LIKE '%${like}%' OR ZTITLE LIKE '%${like}%');`,
+      (error, row: Note) => {
+        if (!error) {
+          notes.push({
+            ZCREATIONDATE: row.ZCREATIONDATE,
+            ZSUBTITLE: row.ZSUBTITLE,
+            ZTEXT: row.ZTEXT,
+            ZTITLE: row.ZTITLE,
+            ZUNIQUEIDENTIFIER: row.ZUNIQUEIDENTIFIER,
+          });
+        }
+      },
+      (error, count) => {
+        if (error) reject(error);
+
+        resolve(notes);
+      }
+    );
+  });
+}
+
+export function getTags() {
+  return new Promise((resolve, reject) => {
+    const tags: string[] = [];
+
+    db.each(
+      "SELECT * FROM ZSFNOTETAG;",
+      (error, row: Tag) => {
+        if (!error) {
+          tags.push(row.ZTITLE);
+        }
+      },
+      (error, count) => {
+        if (error) reject(error);
+
+        resolve(tags);
+      }
+    );
+  });
+}
